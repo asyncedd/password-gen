06ade53ccf5cccc092c6834d1ccdf1ec5545bcb1
asyncedd <i.sync@pissmail.com>
2023-02-22 21:14:19 +0900

    rand + rand.org + anu = <3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use rand::{RngCore, SeedableRng};
-use rand::rngs::StdRng;
+use rand::Rng;
 use reqwest::blocking::Client;
+use serde_json::Value;
 
 fn generate_password(length: usize) -> String {
-    // Generate the first third of the password using a cryptographically secure random number generator
-    let mut rng = StdRng::from_entropy();
-    let mut random_bytes = vec![0u8; length/3];
-    rng.fill_bytes(&mut random_bytes);
-    let password_part1 = hex::encode(random_bytes);
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+{}|[]\\;:'\",./<>?".chars().collect::<Vec<char>>();
+    let mut rng = rand::thread_rng();
+    let password_part1: String = (0..length).map(|_| chars[rng.gen_range(0..chars.len())]).collect();
 
     // Generate the second third of the password using the ANU Quantum Random Generator API
     let client = Client::new();
     let url = format!("https://qrng.anu.edu.au/API/jsonI.php?length={}&type=uint8", length/3);
     let response = client.get(&url).send().unwrap();
-    let data = response.json::<serde_json::Value>().unwrap();
-    let random_bytes = data["data"].as_array().unwrap().iter()
+    let json: Value = serde_json::from_str(&response.text().unwrap()).unwrap();
+    let random_bytes = json["data"].as_array().unwrap().iter()
         .map(|x| x.as_u64().unwrap() as u8)
         .collect::<Vec<u8>>();
     let password_part2 = base64::encode(&random_bytes);
